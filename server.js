fb27942e828b7df5e4d3b46847029bea948945a1
Rose Luu <luuhongnhung10@gmail.com>
2021-10-15 15:13:20 -0500

    PORT fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const apiRoutes = require('./routes/api.js');
 const htmlRoutes = require('./routes/html.js');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const path = require("path")
 const app = express();
 
